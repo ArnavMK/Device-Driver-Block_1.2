c785abb35500d649e9d08b9f1ecf7d583c9493dc
MarkTGeary <Marktgeary@gmail.com>
2026-03-09 13:43:02 +0000

    Driver without callback

--- main_driver.c
+++ main_driver.c
@@ -16,10 +16,10 @@ static const struct usb_device_id controllerArr[] = {
     { }                             
 };
 struct xboxController {
-    struct usb_device *udev;           
-    struct input_dev *input;           
+    struct usb_device *usbDev;           
+    struct input_dev *inputDev;           
     unsigned char *buff;     
-    struct urb *irq_urb;               
+    struct urb *interruptURB;               
 };
 
 static struct usb_driver controller_driver = {
@@ -61,9 +61,11 @@ static void __exit gamepadDriver_exit(void) {
     printk(KERN_INFO "Controller unloaded\n");
 }
 
-static int controller_probe(struct usb_interface *intf, const struct usb_device_id *id) {
-    struct usb_device *udev = interface_to_usbdev(intf);
+static int controller_probe(struct usb_interface *usbInterface, const struct usb_device_id *id) {
+    struct usb_device *usbDev = interface_to_usbdev(usbInterface);
     struct xboxController *controller;
+    struct usb_host_interface *interface_desc;
+    struct usb_endpoint_descriptor *endpoint;
     int error;
     controller = kzalloc(sizeof(struct xboxController), GFP_KERNEL);
     if (!controller) {
@@ -71,67 +73,103 @@ static int controller_probe(struct usb_interface *intf, const struct usb_device_
         return -ENOMEM;
     }
     
-    controller->udev = udev;
-    controller->input = input_allocate_device();
-    if (!controller->input) {
-        printk(KERN_ERR "Could not allocate input device\n");
+    controller->usbDev = usbDev;
+    controller->inputDev = input_allocate_device();
+    if (!controller->inputDev) {
+        printk(KERN_ERR "Could not allocate inputDev device\n");
         kfree(controller);
         return -ENOMEM;
     }
-    controller->input->name = "Xbox Controller";
-    controller->input->phys = "usb/input0";
 
-    input_set_capability(controller->input, EV_KEY, BTN_A);
-    input_set_capability(controller->input, EV_KEY, BTN_B);
-    input_set_capability(controller->input, EV_KEY, BTN_X);
-    input_set_capability(controller->input, EV_KEY, BTN_Y);
-    input_set_capability(controller->input, EV_KEY, BTN_TL);
-    input_set_capability(controller->input, EV_KEY, BTN_TR);
-    input_set_capability(controller->input, EV_KEY, BTN_SELECT);
-    input_set_capability(controller->input, EV_KEY, BTN_START);
+    controller->buff = kzalloc(64, GFP_KERNEL);
+    if(!controller->buff) {
+        printk(KERN_ERR "Could not allocate buffer for controller\n");
+        input_free_device(controller->inputDev);
+        kfree(controller);
+        return -ENOMEM;
+    }
+
+    controller->interruptURB = usb_alloc_urb(0, GFP_KERNEL);
+    if(!controller->interruptURB) {
+        printk(KERN_ERR "Could not allocate URB for controller\n");
+        kfree(controller->buff);
+        input_free_device(controller->inputDev);
+        kfree(controller);
+        return -ENOMEM;
+    }
+
+    controller->inputDev->name = "Xbox Controller";
+    controller->inputDev->phys = "usb/inputDev0";
+
+    input_set_capability(controller->inputDev, EV_KEY, BTN_A);
+    input_set_capability(controller->inputDev, EV_KEY, BTN_B);
+    input_set_capability(controller->inputDev, EV_KEY, BTN_X);
+    input_set_capability(controller->inputDev, EV_KEY, BTN_Y);
+    input_set_capability(controller->inputDev, EV_KEY, BTN_TL);
+    input_set_capability(controller->inputDev, EV_KEY, BTN_TR);
+    input_set_capability(controller->inputDev, EV_KEY, BTN_SELECT);
+    input_set_capability(controller->inputDev, EV_KEY, BTN_START);
     
-    input_set_abs_params(controller->input, ABS_X,  -32768, 32767, 16, 128);
-    input_set_abs_params(controller->input, ABS_Y,  -32768, 32767, 16, 128);
+    input_set_abs_params(controller->inputDev, ABS_X,  -32768, 32767, 16, 128);
+    input_set_abs_params(controller->inputDev, ABS_Y,  -32768, 32767, 16, 128);
 
-    input_set_abs_params(controller->input, ABS_Z,  0, 255, 0, 0);
-    input_set_abs_params(controller->input, ABS_RZ, 0, 255, 0, 0);
+    input_set_abs_params(controller->inputDev, ABS_Z,  0, 255, 0, 0);
+    input_set_abs_params(controller->inputDev, ABS_RZ, 0, 255, 0, 0);
 
 
-    input_set_abs_params(controller->input, ABS_RX, -32768, 32767, 16, 128);
-    input_set_abs_params(controller->input, ABS_RY, -32768, 32767, 16, 128);
+    input_set_abs_params(controller->inputDev, ABS_RX, -32768, 32767, 16, 128);
+    input_set_abs_params(controller->inputDev, ABS_RY, -32768, 32767, 16, 128);
 
-    usb_set_intfdata(intf, controller); 
-    error = input_register_device(controller->input);
+    usb_set_intfdata(usbInterface, controller); 
+    error = input_register_device(controller->inputDev);
+
     if (error) {
-        printk(KERN_ERR "Could not register input device\n");
-        input_free_device(controller->input);
+        printk(KERN_ERR "Could not register inputDev device\n");
+        usb_free_urb(controller->interruptURB);
+        input_free_device(controller->inputDev);
+        kfree(controller->buff);
         kfree(controller);
         return error;
     }
 
-    iface_desc = intf->cur_altsetting;
-    for (i = 0; i < iface_desc->desc.bNumEndpoints; i++) {
-    endpoint = &iface_desc->endpoint[i].desc;
+    interface_desc = usbInterface->cur_altsetting;
+    for (int i = 0; i < interface_desc->desc.bNumEndpoints; i++) {
+        endpoint = &interface_desc->endpoint[i].desc;
 
-    if (usb_endpoint_xfer_int(endpoint) && usb_endpoint_dir_in(endpoint)) {
-        usb_fill_int_urb(controller->irq_urb, udev, usb_rcvintpipe(udev, endpoint->bEndpointAddress), 
-                         controller->buff, 64,
-                         controller_irq_callback, 
-                         controller, 
-                         endpoint->bInterval);
+        if (usb_endpoint_xfer_int(endpoint) && usb_endpoint_dir_in(endpoint)) {
+            usb_fill_int_urb(controller->interruptURB, usbDev, usb_rcvintpipe(usbDev, endpoint->bEndpointAddress), 
+                            controller->buff, 64,
+                            controller_irq_callback, 
+                            controller, 
+                            endpoint->bInterval);
         
-        break; 
+            break; 
+        }
     }
-}
+    int urb_submit_result = usb_submit_urb(controller->interruptURB, GFP_KERNEL);
+    if (urb_submit_result) {
+        printk(KERN_ERR "Could not submit URB for controller\n");
+        input_unregister_device(controller->inputDev);
+        kfree(controller->buff);
+        usb_free_urb(controller->interruptURB);        
+        kfree(controller);
+        return urb_submit_result;
+    }
+
     printk(KERN_INFO "Controller Connected.\n");
     return 0;
 }
 
-static void controller_disconnect(struct usb_interface *intf) {
-    struct xboxController *controller = usb_get_intfdata(intf);
-    input_unregister_device(controller->input);
-    usb_set_intfdata(intf, NULL);
-    kfree(controller);
+static void controller_disconnect(struct usb_interface *usbInterface) {
+    struct xboxController *controller = usb_get_intfdata(usbInterface);
+    usb_set_intfdata(usbInterface, NULL);
+    if (controller) {
+        usb_kill_urb(controller->interruptURB);      
+        usb_free_urb(controller->interruptURB);  
+        kfree(controller->buff);                
+        input_unregister_device(controller->inputDev); 
+        kfree(controller);                       
+    }
     printk(KERN_INFO "Controller Disconnected.\n");
 }
 
